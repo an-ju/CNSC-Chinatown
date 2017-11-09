e1502b8927b6c5039239a19f3992361bfe5a20ba
Xintong Wen <wenb@berkeley.edu>
2017-11-09 23:34:34 +0000

    [chore] started db/seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+points = [{:name => "Old St. Mary's Cathedral", :address => '660 California St.', :description => 'First Asian church in North America', :category => 'Historical'},
+          {:name => "Tianhou Temple", :address => '125 Waverly Place', :description => 'Oldest Buddhist temple in the US', :category => 'Historical'},
+          {:name => "Dragon's Gate", :address => 'Bush St. & Grant Ave.', :description => 'Only authentic Chinatown Gate in North America', :category => 'Attraction'},
+          {:name => "Hong Kong Clay Pot Restaurant", :address => '960 Grant Ave.', :description => 'Chinese clay pot lunch & dinner spot', :category => 'Dining'},
+          {:name => "Hunan Home's Restaurant", :address => '622 Jackson St.', :description => 'Family owned Chinese cooking with booths', :category => 'Dining'},
+          {:name => "Good Mong Kok Bakery", :address => '1039 Stockton St.', :description => 'Homemade dim sum and baked goods', :category => 'Dining'}
+    ]
+
+points.each do |pt|
+  Point.create!(pt)
+end
